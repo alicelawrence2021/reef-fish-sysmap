30b8e079322560fef18ac6036b3b59e7b02fbac4
lucysouthworth <lucy.k.southworth@gmail.com>
2021-07-21 18:09:37 +0000

    changed something

--- fish_sysmap_readcsv.R
+++ fish_sysmap_readcsv.R
@@ -1,4 +1,4 @@
 ## read in data
-setwd('/home/lucy')
+#setwd('/home/lucy')
 library(data.table)
 dat <- fread('ReefFish_data_simple_fromexcel.csv') # fread is faster at reading in large datasets than read.csv
